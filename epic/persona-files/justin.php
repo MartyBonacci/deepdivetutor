fc2ccc9dab2291cdafebf9ea7bb4e41f64be7a3d
Dylan Reuter <djreuter45@gmail.com>
2017-07-29 13:05:15 -0600

    added technology and profession to student persona

--- epic/persona-files/justin.php
+++ epic/persona-files/justin.php
@@ -10,12 +10,15 @@
 		<p><strong>Profession: </strong> Justin has been working as a front desk agent at the Hyatt Regency hotel for
 			the past 3 and a half years. He has an Associates Degree in Hospitality and Tourism from CNM, but is not
 			happy with his profession. Three and a half years at the hotel and he is still in the same position as when
-			he first started. He is looking into a career change and has always been interested in technology. Justin
-			found the Deep Dive Coding Bootcamp's website while browsing the web for different technology fields he
-			could go into. He has always </p>
-		<p><strong>Technology: </strong></p>
+			he first started. He is looking into a career change and has always been interested in technology and coding.
+			Justin found the Deep Dive Coding Bootcamp's website while browsing the web for different technology fields he
+			could go into. He decided to sign up for the bootcamp and is at the end of week 0.</p>
+		<p><strong>Technology: </strong>Justin has a 12" Dell Inspiron Notebook that is about two years old and runs
+			Windows 10. He also has a Samsung Galaxy S7 with the latest version of Android. He is fairly competent on
+			both devices and is often the go to guy for friends and family that have questions about their device.</p>
 		<p><strong>Attitudes &amp; Behaviors: </strong></p>
-		<p><strong>Frustrations &amp; Needs:</strong></p>
+		<p><strong>Frustrations &amp; Needs: </strong>He has always thought coding was interesting, but has doubted his ability to be successful
+			at it due to the fact that always needed more help learning than his classmates did all throughout school.</p>
 		<p><strong>Goals: </strong></p>
 		<!-- maybe put this in user-stories? -->
 		<p><strong>Use Case: </strong></p>
