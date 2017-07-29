d6333880ea1e19dbbca8e8e6106673bcf3ada498
Tim Williams <tikytimbo5@yahoo.com>
2017-07-29 14:22:17 -0600

    added frustrations

--- epic/persona-files/michael.php
+++ epic/persona-files/michael.php
@@ -15,5 +15,16 @@
 		upon the full stack bootcamp at CNM's Stemulus center. After seeing some of the success stories associated with the
 		bootcamp and noting many of the Alumni had zero to minimal coding experience he dicided this was the course for him.
 		Michael Smith is currently in the 3rd week of the program.</p>
+		<p><strong>Technology:</strong> Michael is equipped with a HP desktop computer, he purchase a macbook pro with a
+		14 inch screen for the bootcamp. He uses a iphone 7. Michael is not very familiar with navigating his laptop but
+		has no problem on his phone</p>
+		<p><strong>Attitudes:</strong>Michael has a very positive no quit attitude. when faced with adversity he always goes
+		the extra mile and seeks out any extra resources necessary to complete his goals.</p>
+		<p><strong>Frustrations:</strong>Michael is frustrated with the lack of sleep and family time he has gotten in the
+		first 3 weeks of the bootcamp. He is begining to get frustrated with himself as he feels he is not getting a grasp
+		on the curriculum. Feeling the pressure he decides to look for other resources. He begins watching youtube videos,
+		reading, even listening to podcasts on the way to school in the morning. After spending many hours trying to learn
+		on his own he decides to look into getting one on one tutoring in hopes that a more hands on approach will help him
+		more.</p>
 	</body>
 </html>
