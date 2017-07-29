97aceff00ae3d181dde50d6db1217b343c4e927e
Dylan Reuter <djreuter45@gmail.com>
2017-07-29 14:00:04 -0600

    added frustrations and needs to student persona

--- epic/persona-files/justin.php
+++ epic/persona-files/justin.php
@@ -16,9 +16,28 @@
 		<p><strong>Technology: </strong>Justin has a 12" Dell Inspiron Notebook that is about two years old and runs
 			Windows 10. He also has a Samsung Galaxy S7 with the latest version of Android. He is fairly competent on
 			both devices and is often the go to guy for friends and family that have questions about their device.</p>
-		<p><strong>Attitudes &amp; Behaviors: </strong></p>
-		<p><strong>Frustrations &amp; Needs: </strong>He has always thought coding was interesting, but has doubted his ability to be successful
-			at it due to the fact that always needed more help learning than his classmates did all throughout school.</p>
+		<p><strong>Attitudes &amp; Behaviors: </strong>Justin is the type of student that values interaction. Justin
+			spends a lot of time studying and looking over material because he knows that learning doesn't necessarily
+			come easy to him. He knows that he needs to study more than everyone else in order to be successful. While
+			working on his prework for the bootcamp, Justin relied heavily on Youtube to get a better understanding of
+			the new concepts he was learning. Justin would search through multiple videos, and leave comments asking
+			questions, but most of the videos where dated one or more years back, therefore not closely monitored
+			anymore. Because of this, he never received any replies to his questions. Justin wished he could receive
+			extra help from the bootcamp, either from instructors or former students. He believes they would
+			be the most helpful since they have gone through the class already, and are familiar with the curriculum.</p>
+		<p><strong>Frustrations &amp; Needs: </strong>Justin has at times doubted his ability to be successful
+			in the bootcamp due to the fact that he always needed more help learning than his classmates did all
+			throughout school. When he is faced with new, complex material, he feels frustrated that he doesn't
+			understand it as well as his classmates do. When this happens, Justin will ask the instructor after
+			class to go over a concept again. This does help him, but he still wishes he had more time to spend with
+			the instructor to go over it. This feeling sometimes makes it hard for him to continue and he has at times,
+			thought about quitting. Although after his initial frustrations fade, he knows that he doesn't want to quit
+			and that he just needs extra help learning the material. Justin has used tutors in the past, and thinks that a
+			tutor would be just what he needs to help him grasp the material. However, he is concerned that he will
+			not be able to find one that is qualified to take on the material he is learning from the bootcamp. This is
+			when Justin again turns to Youtube and faces the same problems he had during the prework phase. Justin
+			acknowledges the fact that he needs extra help. He wishes he could sit down with someone more experienced
+			that is willing to take the time necessary to help him learn the material.</p>
 		<p><strong>Goals: </strong></p>
 		<!-- maybe put this in user-stories? -->
 		<p><strong>Use Case: </strong></p>
