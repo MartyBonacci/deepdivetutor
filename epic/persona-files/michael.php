eb3441573df05084749a519143dc0deabde3aca1
Tim Williams <tikytimbo5@yahoo.com>
2017-07-29 13:57:42 -0600

    added proffesional background for Persona Michael Smith

--- epic/persona-files/michael.php
+++ epic/persona-files/michael.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
-		<title></title>
+		<meta charset="utf-8">
+		<title>Persona | michael</title>
 	</head>
 	<body>
-		<p></p>
+		<h1>persona: Michael Smith</h1>
+		<p><strong>Name:</strong> Michael Smith</p>
+		<p><strong>Age:</strong>28</p>
+		<p><strong>Proffesion:</strong> Michael Smith is a security officer at Rust Medical Center in Rio Rancho NM. He has
+		been in the security field for 6 years and is tired of working nights weekends and holidays. After talking with his
+		wife he has decided to change careers. After months of research and thought he decides web developement is a great
+		field that can offer him and his family just what he is looking for. He began looking into to schools and stumbled
+		upon the full stack bootcamp at CNM's Stemulus center. After seeing some of the success stories associated with the
+		bootcamp and noting many of the Alumni had zero to minimal coding experience he dicided this was the course for him.
+		Michael Smith is currently in the 3rd week of the program.</p>
 	</body>
 </html>
