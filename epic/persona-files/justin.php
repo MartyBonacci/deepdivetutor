fde5198b1222dcbe03f3856e5119d57e9bc4c9ab
Dylan Reuter <djreuter45@gmail.com>
2017-07-29 12:34:51 -0600

    started persona for student seeking tutoring

---
+++ epic/persona-files/justin.php
@@ -0,0 +1,23 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Persona | Justin</title>
+	</head>
+	<body>
+		<h2>Persona: Justin Fisher</h2>
+		<p><strong>Name: </strong>Justin Fisher</p>
+		<p><strong>Age: </strong>26</p>
+		<p><strong>Profession: </strong> Justin has been working as a front desk agent at the Hyatt Regency hotel for
+			the past 3 and a half years. He has an Associates Degree in Hospitality and Tourism from CNM, but is not
+			happy with his profession. Three and a half years at the hotel and he is still in the same position as when
+			he first started. He is looking into a career change and has always been interested in technology. Justin
+			found the Deep Dive Coding Bootcamp's website while browsing the web for different technology fields he
+			could go into. He has always </p>
+		<p><strong>Technology: </strong></p>
+		<p><strong>Attitudes &amp; Behaviors: </strong></p>
+		<p><strong>Frustrations &amp; Needs:</strong></p>
+		<p><strong>Goals: </strong></p>
+		<!-- maybe put this in user-stories? -->
+		<p><strong>Use Case: </strong></p>
+	</body>
+</html>
