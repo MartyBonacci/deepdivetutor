277486e948538aaed1894a3730d2a7fefaeb2fc6
Godfrey Davis <gdavis42@cnm.edu>
2017-07-30 16:52:04 -0600

    its not finished thought id commit what i had so far

---
+++ epic/persona-files/danielle.php
@@ -0,0 +1,21 @@
+​<!DOCTYPE html>
+<html>
+	 <title>Persona Danielle</title>
+</html>
+
+<body>
+	<h1>Persona Danielle</h1>
+	<ul>
+		<li><strong>Name:</strong>Danielle</li>
+		<li><strong>Age:</strong>32</li>
+		<li><strong>Profession/Role:</strong>A biologist who enrolled and graduated from deep dive bootcamp as a full-stack
+		developer.Currently tutors a student from the cohort in progress</li>
+		<li><strong>Technology:</strong> Pc laptop, slack, Smartphone, internet</li>
+		<li><strong>Behavior and Attitudes:</strong> She's new to tutoring. But she has the experience of being fresh out
+		of coding bootcamp. Right now she doesn't ask directly for a rate she just take what's given to her.
+		She's available for tutoring at the most, three days a week and her sessions usually last 1-2hours.Not looking at tutoring as a fulltime
+		job but something to add to income and enrich her own understanding of coding. very easy to work with</li>
+		<li><strong>Goal:</strong></li>Not to necessarily get as many people to tutor but atleast on the the three days she has available, and to
+		also have a set price
+	</ul>
+</body>
