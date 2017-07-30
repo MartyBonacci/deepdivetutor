983c01b6d6c5bc5934f42f69fbbfac103f624e67
Dylan Reuter <djreuter45@gmail.com>
2017-07-29 20:46:13 -0600

    added user story for Justin

---
+++ epic/user-stories.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>User Stories</title>
+	</head>
+	<body>
+		<h1>User Stories</h1>
+		<h2>Justin:</h2>
+		<p>As a user I need to be able to find a tutor that went through the bootcamp, that is qualified to assist me in
+			MySQL, because I know I need extra help outside of the classroom on that topic.</p>
+	</body>
+</html>
