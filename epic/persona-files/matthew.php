c8d88340fbeee0362756c2cee6436e18edc5555f
Marty Bonacci <martybonacci@gmail.com>
2017-07-31 01:18:35 -0600

    created persona for Matthew

---
+++ epic/persona-files/matthew.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Marty Bonacci">
+		<meta name="description" content="Persona and Use Case for Matthew">
+		<title>Persona for Matthew</title>
+		<!-- custom CSS stylesheet -->
+		<link rel="stylesheet" href="css/style.css" type="text/css" />
+	</head>
+	<body>
+		<h1>Persona for Matthew Bretton, 32</h1>
+		<h2>Role:</h2>
+		<p>Matthew graduated from the Deep Dive Coding Bootcamp in early 2016 and found a job within a month doing back end, PHP and MySQL database work for an Albuquerque marketing company. He enjoys his job and gets paid well.</p>
+		<h2>Technology:</h2>
+		<p>Matthew still has the HP laptop that he bought for the coding bootcamp but he upgraded to Windows 10, and dual booted Linux. Even though he spends the workday on a laptop or desktop, Matthew prefers the mobility of his Samsung Galaxy S9 when not at work.</p>
+		<h2>Attitude and Behavior:</h2>
+		<p>Matthew enjoyed learning to code and worked hard to get the most out of the coding bootcamp. Even with this hard work, Matthew experienced some difficulty with the material in the bootcamp. Fortunately Matthew was able to work through those challenges. He feels that he has a little extra insight into the difficulties that a student may have when learning full stack web development.</p>
+		<h2>Frustrations and Needs:</h2>
+		<p>Although Matthew is making good money at his job, his school loan payments are high. He would like to find a way to make some extra money using his skills. Matthew would also like to help others that might be experiencing the same difficulties that he had.</p>
+		<h2>Goals:</h2>
+		<p>Matthew wants to find a tutoring gig that matches his education and experience.</p>
+		<h2>Use Case:</h2>
+		<p>Matthew stopped by the DDCB to say hello to Dylan and the crew and learned of the greatest website in existence, deepdivetutor.com. Later that day Matthew went to deepdivetutor.com on his android phone. He’s curious to see what tutoring demand there may be and how many tutors are offering their services. Luckily for Matthew the site was designed with the mobile first methodology and was easy for him to navigate on his phone. Not only was it easy for him to search for students that needed help with PHP and MySQL (what he worked with everyday), but it was also easy to see how he measured against the other tutors.</p>
+		<p>Matthew created an account profile explaining his experience, what he wanted to teach, his rates and also uploaded a picture. Matthew knew that it may be harder to get his first client because he didn’t have a rating on the site yet. Fortunately Matthew was able to upload a recommendation from the DDCB staff to improve prospective student confidence in him.</p>
+		<p>Within 48 hours Matthew was contacted by a student that needed help with object oriented PHP and set his first tutoring appointment...</p>
+	</body>
+</html>
