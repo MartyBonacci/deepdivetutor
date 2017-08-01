28b6832dfe318685fc49ddf76a111026363d7ce9
Dylan Reuter <djreuter45@gmail.com>
2017-08-01 15:23:24 -0600

    added starter conceptual model

---
+++ epic/conceptual-model.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+	</head>
+	<body>
+		<h1>Conceptual Model</h1>
+		<p>Profile:</p>
+		<ul>
+			<li>profileId</li>
+			<li>profileName</li>
+			<li>profileEmail</li>
+			<li>profilePhoto</li>
+			<li>profileStudent</li>
+			<li>profileTutor</li>
+			<li>profileActivationToken</li>
+			<li>profileHash</li>
+			<li>profileSalt</li>
+		</ul>
+	</body>
+</html>
