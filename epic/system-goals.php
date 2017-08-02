ae93e2f5f6fbf5923ada1a0595414e78e507ea8a
Dylan Reuter <djreuter45@gmail.com>
2017-08-01 19:47:14 -0600

    added system goals

---
+++ epic/system-goals.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>System Goals</title>
+	</head>
+	<body>
+		<h1>System Goals</h1>
+		<p>By using deepdivetutor.com the user will be able to:</p>
+		<ul>
+			<li>Create an account to receive tutoring or be the tutor.</li>
+			<li>Connect with other Deep Dive Coding students by receiving 1 on 1 assistance from alumni.</li>
+			<li>Enhance their learning to maximize their success in the class.</li>
+			<li>Review any course concepts they need clarification or further assistance with.</li>
+			<li>Receive quality tutoring from former students who have gone through the same rigorous curriculum.</li>
+		</ul>
+	</body>
+</html>
