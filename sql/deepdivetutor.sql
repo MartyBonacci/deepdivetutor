2de15204637525facf87eec0ce5a6a7a1f1b68cf
Dylan Reuter <djreuter45@gmail.com>
2017-08-02 16:49:19 -0600

    added profile entity to ddl

--- sql/deepdivetutor.sql
+++ sql/deepdivetutor.sql
@@ -6,3 +6,21 @@ DROP TABLE IF EXISTS reviews;
 DROP TABLE IF EXISTS profile;
 
 -- create tables
+CREATE TABLE profile (
+	-- NOT NULL means they are required
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileName VARCHAR(50) NOT NULL,
+	profileEmail VARCHAR(128) NOT NULL,
+	-- true is 1 false is 0
+	profileType BIT DEFAULT 1 NOT NULL,
+	-- profileGithubToken
+	profileBio VARCHAR(500) NOT NULL,
+	profileRate INT UNSIGNED NOT NULL,
+	profileImage
+	profileLastEditDate DATETIME(6) NOT NULL,
+	profileActivationToken CHAR(32) NOT NULL,
+	profileHash CHAR(128) NOT NULL,
+	profileSalt CHAR(64) NOT NULL,
+	UNIQUE (profileEmail),
+	PRIMARY KEY (profileId)
+);
