58c414e02f1287693a58cb5749c7bc8c6e7e8f65
Dylan Reuter <djreuter45@gmail.com>
2017-08-02 17:00:06 -0600

    re-formated code

--- sql/deepdivetutor.sql
+++ sql/deepdivetutor.sql
@@ -8,20 +8,20 @@ DROP TABLE IF EXISTS profile;
 -- create tables
 CREATE TABLE profile (
 	-- NOT NULL means they are required
-	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	profileName VARCHAR(50) NOT NULL,
-	profileEmail VARCHAR(128) NOT NULL,
+	profileId              INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileName            VARCHAR(50)                 NOT NULL,
+	profileEmail           VARCHAR(128)                NOT NULL,
 	-- true is 1 false is 0
-	profileType BIT DEFAULT 1 NOT NULL,
+	profileType            BIT DEFAULT 1               NOT NULL,
 	-- profileGithubToken
-	profileBio VARCHAR(500) NOT NULL,
-	profileRate INT UNSIGNED NOT NULL,
+	profileBio             VARCHAR(500)                NOT NULL,
+	profileRate            INT UNSIGNED                NOT NULL,
 	-- not sure if image is right
-	profileImage BLOB NULL,
-	profileLastEditDate DATETIME(6) NOT NULL,
-	profileActivationToken CHAR(32) NOT NULL,
-	profileHash CHAR(128) NOT NULL,
-	profileSalt CHAR(64) NOT NULL,
+	profileImage           BLOB                        NULL,
+	profileLastEditDate    DATETIME(6)                 NOT NULL,
+	profileActivationToken CHAR(32)                    NOT NULL,
+	profileHash            CHAR(128)                   NOT NULL,
+	profileSalt            CHAR(64)                    NOT NULL,
 	UNIQUE (profileEmail),
 	PRIMARY KEY (profileId)
 );
