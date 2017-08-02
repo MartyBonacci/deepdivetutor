a1d07731e15337109b6af48485243e1b874edcf6
Dylan Reuter <djreuter45@gmail.com>
2017-08-02 16:20:35 -0600

    started ddl

---
+++ sql/deepdivetutor.sql
@@ -0,0 +1,8 @@
+-- purge tables
+-- drop tables in reverse order
+DROP TABLE IF EXISTS profileSkill;
+DROP TABLE IF EXISTS skill;
+DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS profile;
+
+-- create tables
