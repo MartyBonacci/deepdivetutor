31f7a67be8e4457ce5ec6a361a22ba4e555015ae
Dylan Reuter <djreuter45@gmail.com>
2017-08-01 19:22:01 -0600

    added executive summary

---
+++ epic/executive-summary.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Executive Summary</title>
+	</head>
+	<body>
+		<h1>Executive Summary</h1>
+		<p>Deep Dive Tutor. The oxygen tank for when you dive deep and need some extra air.</p>
+	</body>
+</html>
