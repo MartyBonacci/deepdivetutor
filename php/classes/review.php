1cf50d1842d0ca90f6d684d0b3042f8fc6268bfa
Tim Williams <tikytimbo5@yahoo.com>
2017-08-03 16:44:32 -0600

    added reviewId, reviewStudentProfileId, reviewTutorProfileId, reviewRating https://app.asana.com/0/333821720990064/401465255199790/f

--- php/classes/review.php
+++ php/classes/review.php
@@ -1,1 +1,33 @@
 <?php
+
+namespace Edu\Cnm\DeepDiveTutor;
+require_once ("autoload.php");
+
+/**
+ * <h1>deep dive tutor</h1>
+ * @author Timothy Williams <tkotalik@cnm.edu>
+ * @version 1.0
+ **/
+class review {
+	/**
+	 * primary key for profileId
+	 * @var int reviewId
+	 **/
+	private $reviewId;
+	/**
+	 * id of the student that saved this review; this is a foreign key
+	 * var int $reviewStudentProfileId
+	 **/
+	private $reviewStudentProfileId;
+	/**
+	 * id of the tutor that saved this review; this is a foreign key
+	 * var int $reviewTutorProfileId
+	 **/
+	private $reviewTutorProfileId;
+	/**
+	 * actual rating of tutor
+	 * var int $reviewRating
+	 **/
+	private $reviewRating;
+
+}
