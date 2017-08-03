9acb3345528973d777de9633fee1705f8ac5c295
Dylan Reuter <djreuter45@gmail.com>
2017-08-03 15:01:46 -0600

    changes to ddl

--- sql/deepdivetutor.sql
+++ sql/deepdivetutor.sql
@@ -11,17 +11,15 @@ CREATE TABLE profile (
 	profileId              INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	profileName            VARCHAR(50)                 NOT NULL,
 	profileEmail           VARCHAR(128)                NOT NULL,
-	-- true is 1 false is 0
-	profileType            BIT DEFAULT 1               NOT NULL,
-	-- profileGithubToken
+	profileType            TINYINT UNSIGNED            NOT NULL,
+	profileGithubToken     VARCHAR(64),
 	profileBio             VARCHAR(500)                NOT NULL,
-	profileRate            INT UNSIGNED                NOT NULL,
-	-- not sure if image is right
-	profileImage           BLOB                        NULL,
-	profileLastEditDate    DATETIME(6)                 NOT NULL,
-	profileActivationToken CHAR(32)                    NOT NULL,
-	profileHash            CHAR(128)                   NOT NULL,
-	profileSalt            CHAR(64)                    NOT NULL,
+	profileRate            DECIMAL(5, 2),
+	profileImage           VARCHAR(32),
+	profileLastEditDate    TIMESTAMP(6)                NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+	profileActivationToken CHAR(32),
+	profileHash            CHAR(128),
+	profileSalt            CHAR(64),
 	UNIQUE (profileEmail),
 	PRIMARY KEY (profileId)
 );
@@ -32,7 +30,7 @@ CREATE TABLE review (
 	reviewStudentProfileId INT UNSIGNED                NOT NULL,
 	reviewTutorProfileId   INT UNSIGNED                NOT NULL,
 	-- not sure about reviewRating
-	reviewRating           INT(5)                      NOT NULL,
+	reviewRating           TINYINT UNSIGNED            NOT NULL,
 	reviewText             VARCHAR(500)                NOT NULL,
 	-- should reviewDate be reviewDateTime?
 	reviewDate             TIMESTAMP(6)                NOT NULL,
