d71ad00dd0fb6aca2fc2b2edb26943033be6c83a
Marty Bonacci <martybonacci@gmail.com>
2017-08-03 13:21:58 -0600

    delete s

--- sql/deepdivetutor.sql
+++ sql/deepdivetutor.sql
@@ -2,7 +2,7 @@
 -- drop tables in reverse order
 DROP TABLE IF EXISTS profileSkill;
 DROP TABLE IF EXISTS skill;
-DROP TABLE IF EXISTS reviews;
+DROP TABLE IF EXISTS review;
 DROP TABLE IF EXISTS profile;
 
 -- create tables
