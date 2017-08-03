2c95c14a6c544024c08e0ab18aa3217fbbb8220b
Marty Bonacci <martybonacci@gmail.com>
2017-08-03 17:04:24 -0600

    fixed typos in ProfileSkill.php https://app.asana.com/0/333821720990064/list

--- php/classes/ProfileSkill.php
+++ php/classes/ProfileSkill.php
@@ -11,7 +11,7 @@ require_once("autoload.php");
  * @author MartyBonacci <mbonacci@@cnm.edu>
  * @version 1.0.0
  **/
-class profileSkill implements \JsonSerializable {
+class profileSkill {
 
 	/**
 	 * FOREIGN KEY for profile (profileId),
@@ -27,18 +27,18 @@ class profileSkill implements \JsonSerializable {
 	/**
 	 * constructor for this profileSkill
 	 *
-	 * @param int $newprofileSkillprofileId of the profile that has the skill
-	 * @param int $newprofileSkillSkillId of the skill that the profile has
+	 * @param int $newProfileSkillprofileId of the profile that has the skill
+	 * @param int $newProfileSkillSkillId of the skill that the profile has
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
 	 * @throws \TypeError if data types violate type hints
 	 * @throws \Exception if some other exception occurs
 	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
 	 **/
-	public function __construct(?int $newprofileSkillprofileId, int $newprofileSkillSkillId) {
+	public function __construct(?int $newProfileSkillprofileId, int $newProfileSkillSkillId) {
 		try {
-			$this->setprofileSkillprofileId($newprofileSkillprofileId);
-			$this->setprofileSkillSkillId($newprofileSkillSkillId);
+			$this->setProfileSkillprofileId($newProfileSkillprofileId);
+			$this->setProfileSkillSkillId($newProfileSkillSkillId);
 		}
 			//determine what exception type was thrown
 		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
@@ -53,26 +53,26 @@ class profileSkill implements \JsonSerializable {
 	 *
 	 * @return int value of FOREIGN KEY for profile (profileId)
 	 **/
-	public function getprofileSkillprofileId() : int{
+	public function getProfileSkillprofileId() : int{
 		return($this->profileSkillprofileId);
 	}
 
 	/**
 	 * mutator method for FOREIGN KEY for profile (profileId)
 	 *
-	 * @param int $newprofileSkillprofileId new value of FOREIGN KEY for profile (profileId)
-	 * @throws \RangeException if $newPprofileSkillprofileId is not positive
-	 * @throws \TypeError if $newprofileSkillprofileId is not an integer
+	 * @param int $newProfileSkillprofileId new value of FOREIGN KEY for profile (profileId)
+	 * @throws \RangeException if $newProfileSkillprofileId is not positive
+	 * @throws \TypeError if $newProfileSkillprofileId is not an integer
 	 **/
-	public function setprofileSkillprofileId(int $newprofileSkillprofileId) : void {
+	public function setProfileSkillprofileId(int $newProfileSkillprofileId) : void {
 
 		// verify the profile id is positive
-		if($newprofileSkillprofileId <= 0) {
-			throw(new \RangeException("tweet profile id is not positive"));
+		if($newProfileSkillprofileId <= 0) {
+			throw(new \RangeException("skill profile id is not positive"));
 		}
 
 		// convert and store the profile id
-		$this->profileSkillprofileId = $newprofileSkillprofileId;
+		$this->profileSkillprofileId = $newProfileSkillprofileId;
 	}
 
 
@@ -85,26 +85,26 @@ class profileSkill implements \JsonSerializable {
 	 *
 	 * @return int value of FOREIGN KEY for skill (skillId)
 	 **/
-	public function getprofileSkillSkillId() : int{
+	public function getProfileSkillSkillId() : int{
 		return($this->profileSkillSkillId);
 	}
 
 	/**
 	 * mutator method for FOREIGN KEY for skill (skillId)
 	 *
-	 * @param int $newprofileSkillSkillId new value of profileSkillSkillId
-	 * @throws \RangeException if $newprofileSkillSkillId is not positive
-	 * @throws \TypeError if $newprofileSkillSkillId is not an integer
+	 * @param int $newProfileSkillSkillId new value of profileSkillSkillId
+	 * @throws \RangeException if $newProfileSkillSkillId is not positive
+	 * @throws \TypeError if $newProfileSkillSkillId is not an integer
 	 **/
-	public function setprofileSkillSkillId(int $newprofileSkillSkillId) : void {
+	public function setProfileSkillSkillId(int $newProfileSkillSkillId) : void {
 
 		// verify the skill id is positive
-		if($newprofileSkillSkillId <= 0) {
-			throw(new \RangeException("tweet profile id is not positive"));
+		if($newProfileSkillSkillId <= 0) {
+			throw(new \RangeException("profile skill id is not positive"));
 		}
 
 		// convert and store the skill id
-		$this->profileSkillSkillId = $newprofileSkillSkillId;
+		$this->profileSkillSkillId = $newProfileSkillSkillId;
 	}
 
 
@@ -121,20 +121,5 @@ class profileSkill implements \JsonSerializable {
 
 
 
-
-
-
-
-	/**
-	 * formats the state variables for JSON serialization
-	 *
-	 * @return array resulting state variables to serialize
-	 **/
-	public function jsonSerialize() {
-		$fields = get_object_vars($this);
-		//format the date so that the front end can consume it
-		$fields["tweetDate"] = round(floatval($this->tweetDate->format("U.u")) * 1000);
-		return($fields);
-	}
 }
 
