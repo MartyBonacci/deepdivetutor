124b8807f3d77687b19c0dfede8c1d8ffb6dbda2
Tim Williams <tikytimbo5@yahoo.com>
2017-08-07 11:40:11 -0600

    added json https://app.asana.com/0/333821720990064/401465255199790/f

--- php/classes/Review.php
+++ php/classes/Review.php
@@ -466,7 +466,18 @@ class Review {
 		return ($reviews);
 	}
 
+	/**
+	 * formats the state variables for JSON serialization
+	 *
+	 * @return array resulting state variables to serialize
+	 **/
 
+	public function jsonSerialize() {
+		$fields = get_object_vars($this);
+		// format the date so that the front end can consume it
+		$fields["reviewDatetime"] = round(floatval($this->reviewDateTime->format("U.u")) * 1000);
+		return($fields);
+	}
 }
 
 
