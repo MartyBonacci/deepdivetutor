ae7feea06398872658acd508ca266933bd91f7dc
Marty Bonacci <martybonacci@gmail.com>
2017-08-07 16:57:25 -0600

    adding pdo to ProfileSkill.php https://app.asana.com/0/333821720990064/list

--- php/classes/ProfileSkill.php
+++ php/classes/ProfileSkill.php
@@ -1,8 +1,10 @@
 <?php
+
 namespace Edu\Cnm\DataDesign;
 
 require_once("autoload.php");
 /*item class*/
+
 /**
  * Create profileSkill class
  *
@@ -11,7 +13,7 @@ require_once("autoload.php");
  * @author MartyBonacci <mbonacci@@cnm.edu>
  * @version 1.0.0
  **/
-class profileSkill {
+class profileSkill implements \JsonSerializable {
 
 	/**
 	 * FOREIGN KEY for profile (profileId),
@@ -27,8 +29,8 @@ class profileSkill {
 	/**
 	 * constructor for this profileSkill
 	 *
-	 * @param int $newProfileSkillprofileId of the profile that has the skill
-	 * @param int $newProfileSkillSkillId of the skill that the profile has
+	 * @param int and not null $newProfileSkillprofileId of the profile that has the skill
+	 * @param int and not null $newProfileSkillSkillId of the skill that the profile has
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
 	 * @throws \TypeError if data types violate type hints
@@ -39,8 +41,7 @@ class profileSkill {
 		try {
 			$this->setProfileSkillprofileId($newProfileSkillprofileId);
 			$this->setProfileSkillSkillId($newProfileSkillSkillId);
-		}
-			//determine what exception type was thrown
+		} //determine what exception type was thrown
 		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
 			$exceptionType = get_class($exception);
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
@@ -53,8 +54,8 @@ class profileSkill {
 	 *
 	 * @return int value of FOREIGN KEY for profile (profileId)
 	 **/
-	public function getProfileSkillprofileId() : int{
-		return($this->profileSkillprofileId);
+	public function getProfileSkillprofileId(): int {
+		return ($this->profileSkillprofileId);
 	}
 
 	/**
@@ -64,7 +65,7 @@ class profileSkill {
 	 * @throws \RangeException if $newProfileSkillprofileId is not positive
 	 * @throws \TypeError if $newProfileSkillprofileId is not an integer
 	 **/
-	public function setProfileSkillprofileId(int $newProfileSkillprofileId) : void {
+	public function setProfileSkillprofileId(int $newProfileSkillprofileId): void {
 
 		// verify the profile id is positive
 		if($newProfileSkillprofileId <= 0) {
@@ -76,17 +77,13 @@ class profileSkill {
 	}
 
 
-
-
-
-
 	/**
 	 * accessor method for FOREIGN KEY for skill (skillId)
 	 *
 	 * @return int value of FOREIGN KEY for skill (skillId)
 	 **/
-	public function getProfileSkillSkillId() : int{
-		return($this->profileSkillSkillId);
+	public function getProfileSkillSkillId(): int {
+		return ($this->profileSkillSkillId);
 	}
 
 	/**
@@ -96,7 +93,8 @@ class profileSkill {
 	 * @throws \RangeException if $newProfileSkillSkillId is not positive
 	 * @throws \TypeError if $newProfileSkillSkillId is not an integer
 	 **/
-	public function setProfileSkillSkillId(int $newProfileSkillSkillId) : void {
+	public function setProfileSkillSkillId(int $newProfileSkillSkillId): void {
+
 
 		// verify the skill id is positive
 		if($newProfileSkillSkillId <= 0) {
@@ -108,6 +106,29 @@ class profileSkill {
 	}
 
 
+	/**
+	 * inserts this profileSkill into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo): void {
+
+		//throw an exception if either foreign key is null
+		if($this->profileSkillprofileId === null || $this->profileSkillSkillId === null) {
+			throw(new \PDOException("not a new profileSkillprofileId or profileSkillSkillId"));
+		}
+
+
+
+		// create query template
+		$query = "INSERT INTO profileSkill(profileSkillprofileId, profileSkillSkillId) VALUES(:profileSkillprofileId, :profileSkillSkillId)";
+		$statement = $pdo->prepare($query);
+
+		$parameters = ["profileSkillprofileId" => $this->profileSkillprofileId, "profileSkillSkillId" => $this->profileSkillSkillId];
+		$statement->execute($parameters);
+	}
 
 
 
@@ -119,7 +140,128 @@ class profileSkill {
 
 
 
+	/**
+	 * deletes this Tweet from mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo): void {
+		// enforce the tweetId is not null (i.e., don't delete a tweet that hasn't been inserted)
+		if($this->tweetId === null) {
+			throw(new \PDOException("unable to delete a tweet that does not exist"));
+		}
 
+		// create query template
+		$query = "DELETE FROM tweet WHERE tweetId = :tweetId";
+		$statement = $pdo->prepare($query);
 
+		// bind the member variables to the place holder in the template
+		$parameters = ["tweetId" => $this->tweetId];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * updates this Tweet in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function update(\PDO $pdo): void {
+		// enforce the tweetId is not null (i.e., don't update a tweet that hasn't been inserted)
+		if($this->tweetId === null) {
+			throw(new \PDOException("unable to update a tweet that does not exist"));
+		}
+
+		// create query template
+		$query = "UPDATE tweet SET tweetProfileId = :tweetProfileId, tweetContent = :tweetContent, tweetDate = :tweetDate WHERE tweetId = :tweetId";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holders in the template
+		$formattedDate = $this->tweetDate->format("Y-m-d H:i:s.u");
+		$parameters = ["tweetProfileId" => $this->tweetProfileId, "tweetContent" => $this->tweetContent, "tweetDate" => $formattedDate, "tweetId" => $this->tweetId];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * gets the Tweet by tweetId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $tweetId tweet id to search for
+	 * @return Tweet|null Tweet found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getTweetByTweetId(\PDO $pdo, int $tweetId): ?Tweet {
+		// sanitize the tweetId before searching
+		if($tweetId <= 0) {
+			throw(new \PDOException("tweet id is not positive"));
+		}
+
+		// create query template
+		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet WHERE tweetId = :tweetId";
+		$statement = $pdo->prepare($query);
+
+		// bind the tweet id to the place holder in the template
+		$parameters = ["tweetId" => $tweetId];
+		$statement->execute($parameters);
+
+		// grab the tweet from mySQL
+		try {
+			$tweet = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$tweet = new Tweet($row["tweetId"], $row["tweetProfileId"], $row["tweetContent"], $row["tweetDate"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($tweet);
+	}
+
+	/**
+	 * gets the Tweet by profile id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $tweetProfileId profile id to search by
+	 * @return \SplFixedArray SplFixedArray of Tweets found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getTweetByTweetProfileId(\PDO $pdo, int $tweetProfileId): \SPLFixedArray {
+		// sanitize the profile id before searching
+		if($tweetProfileId <= 0) {
+			throw(new \RangeException("tweet profile id must be positive"));
+		}
+		// create query template
+		$query = "SELECT tweetId, tweetProfileId, tweetContent, tweetDate FROM tweet WHERE tweetProfileId = :tweetProfileId";
+		$statement = $pdo->prepare($query);
+		// bind the tweet profile id to the place holder in the template
+		$parameters = ["tweetProfileId" => $tweetProfileId];
+		$statement->execute($parameters);
+		// build an array of tweets
+		$tweets = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$tweet = new Tweet($row["tweetId"], $row["tweetProfileId"], $row["tweetContent"], $row["tweetDate"]);
+				$tweets[$tweets->key()] = $tweet;
+				$tweets->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($tweets);
+	}
+
+	public function jsonSerialize() {
+		$fields = get_object_vars($this);
+		return ($fields);
+	}
 }
 
