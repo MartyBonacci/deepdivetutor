f2be1e570047cbe771f21dccddf35f5e58db611b
Tim Williams <tikytimbo5@yahoo.com>
2017-08-08 11:53:19 -0600

    started Unit Test for review class https://app.asana.com/0/333821720990064/401465255199790/f

---
+++ php/classes/Test/ReviewTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Edu\Cnm\DeepDiveTutor\Test;
+
+use Edu\Cnm\DeepDiveTutor\{review};
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__). "autoload.php");
+
+/**
+ * Full PHPUnit test for the review class
+ *
+ * This is a complete PHPUnit test of the review class. It is complete because *ALL* mySQL/PDO enabled methods are tested
+ * for both invalid and valid inputs.
+ *
+ * @see review
+ * @author Timothy Williams <tkotalik@cnm.edu>
+ **/
+
