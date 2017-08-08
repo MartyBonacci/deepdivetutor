6ea275569f710a3953d302e33ddc04992bf1e6b4
Tim Williams <tikytimbo5@yahoo.com>
2017-08-08 13:08:42 -0600

    continued Unit Test for review class https://app.asana.com/0/333821720990064/401465255199790/f

--- php/classes/Test/ReviewTest.php
+++ php/classes/Test/ReviewTest.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace Edu\Cnm\DeepDiveTutor\Test;
 
-use Edu\Cnm\DeepDiveTutor\{review};
+use Edu\Cnm\DeepDiveTutor\{
+	Profile, Review
+};
 
 // grab the class under scrutiny
-require_once(dirname(__DIR__). "autoload.php");
+require_once(dirname(__DIR__) . "autoload.php");
 
 /**
  * Full PHPUnit test for the review class
@@ -15,4 +18,33 @@ require_once(dirname(__DIR__). "autoload.php");
  * @see review
  * @author Timothy Williams <tkotalik@cnm.edu>
  **/
+class ReviewTest extends DeepDiveTutor {
+	/**
+	 *profile that created the review; this is for foreign key relations
+	 * @var Profile profile
+	 **/
 
+	protected $profile = null;
+
+	/**
+	 * valid profile hash to create the profile object to own the test
+	 * @var $VALID_HASH
+	 **/
+
+	protected $VALID_PROFILE_HASH;
+
+	/**
+	 * valid salt to use to create the profile object to own the test
+	 * @var string $VALID_SALT
+	 **/
+
+	protected $VALID_PROFILE_SALT;
+
+	/**
+	 * content of review
+	 * @var string $VALID_REVIEWCONTENT
+	 **/
+
+	protected $VALID_REVIEWCONTENT = "PHPUnit test passing";
+
+}
