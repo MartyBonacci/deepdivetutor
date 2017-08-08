78f19f7886a00ff6632ea23a7d9f690897272255
Dylan Reuter <djreuter45@gmail.com>
2017-08-08 13:06:26 -0600

    added phpunit config file https://app.asana.com/0/333821720990064/401465255199789

---
+++ php/classes/Test/ProfileTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace Edu\Cnm\DeepDiveTutor\Test;
+
+use Edu\Cnm\DeepDiveTutor\{Profile};
+
+// grab the class
+require_once(dirname(__DIR__) . "/autoload.php");
+
+/**
+ * Full PHPUnit test for the Profile class
+ *
+ *
+ */
