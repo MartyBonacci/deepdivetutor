946ee18f7d92d5cdd3f8ac47c37aa8357a8e377d
Dylan Reuter <djreuter45@gmail.com>
2017-08-09 10:33:59 -0600

    wrote test for insert profile https://app.asana.com/0/333821720990064/401465255199789

--- php/classes/Test/ProfileTest.php
+++ php/classes/Test/ProfileTest.php
@@ -62,12 +62,80 @@ class ProfileTest extends DeepDiveTutorTest {
 	 * valid profile github token
 	 * @var $VALID_GITHUBTOKEN
 	 */
-	protected $VALID_GITHUBTOKEN;
+	protected $VALID_GITHUBTOKEN = "Loremipsumdolorsitametconsecteturadipiscingelitposuerefhdrtuiseb";
 
 	/**
 	 * valid profile rate
 	 * @var float $VALID_RATE
 	 */
-	protected $VALID_RATE =
+
+	protected $VALID_RATE = 25.00;
+
+	/**
+	 * valid profile image
+	 * @var $VALID_IMAGE
+	 */
+	protected $VALID_IMAGE = "Loremipsdolorsitametconthirtytwo";
+
+	/**
+	 * valid last edit date time
+	 * @var $VALID_DATETIME
+	 */
+	protected $VALID_DATETIME = null;
+
+	/**
+	 * run the default operation to create the salt and hash
+	 */
+	public final function setUp(): void {
+		// refer to name of the base class as given in the extends declaration of this class
+		parent::setUp();
+
+		//
+		$password = "abc123";
+		$this->VALID_SALT = bin2hex(random_bytes(32));
+		$this->VALID_HASH = hash_pbkdf2("sha512", $password, $this->VALID_SALT, 262144);
+		$this->VALID_ACTIVATION = bin2hex(random_bytes(16));
+	}
+
+	/**
+	 * test inserting a valid Profile and verify that the actual MySQL data matches
+	 */
+	public function testInsertValidProfile(): void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("profile");
+
+		// create a new Profile and insert it into MySQL
+	$profile = new Profile(null, $this->VALID_NAME, $this->VALID_EMAIL, $this->VALID_TYPE_S, $this->VALID_GITHUBTOKEN,
+		$this->VALID_BIO, $this->VALID_RATE, $this->VALID_IMAGE, $this->VALID_DATETIME, $this->VALID_ACTIVATION,
+		$this->VALID_HASH, $this->VALID_SALT);
+
+		// var_dump($profile);
+
+		$profile->insert($this->getPDO());
+
+		// grab the data from MySQL and enforce the fields match out expectations
+		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
+		$this->assertEquals($pdoProfile->getProfileName(), $this->VALID_NAME);
+		$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_EMAIL);
+		$this->assertEquals($pdoProfile->getProfileType(), $this->VALID_TYPE_S);
+		$this->assertEquals($pdoProfile->getProfileGithubToken(), $this->VALID_GITHUBTOKEN);
+		$this->assertEquals($pdoProfile->getProfileBio(), $this->VALID_BIO);
+		$this->assertEquals($pdoProfile->getProfileRate(), $this->VALID_RATE);
+		$this->assertEquals($pdoProfile->getProfileImage(), $this->VALID_IMAGE);
+		$this->assertEquals($pdoProfile->getProfileLastEditDateTime(), $this->VALID_DATETIME);
+		$this->assertEquals($pdoProfile->getProfileActivationToken(), $this->VALID_ACTIVATION);
+		$this->assertEquals($pdoProfile->getProfileHash(), $this->VALID_HASH);
+		$this->assertEquals($pdoProfile->getProfileSalt(), $this->VALID_SALT);
+	}
+
+	/**
+	 * test inserting a Profile that already exists
+	 *
+	 * @expectedException \PDOException
+	 */
+	
+
+
 
 }
