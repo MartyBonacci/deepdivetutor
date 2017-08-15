072db8c730d2d0a991bbbc9878e773136c7207c0
Tim Williams <tikytimbo5@yahoo.com>
2017-08-15 15:21:20 -0600

    "grab the mySQL connection" https://app.asana.com/0/333821720990064/401465255199790/f

--- public_html/api/review/index.php
+++ public_html/api/review/index.php
@@ -6,7 +6,9 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once ("etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\DeepDiveTutor\ {
-	Review
+	Review,
+//only use the profile class for testing purposes
+profile
 };
 
 /**
@@ -28,4 +30,30 @@ $reply->data = null;
 try {
 	//grab the mySQL connection
 	$pdo = connectToEncrptedMySQL("/etc/apache2/capstone-mysql-deepdivetutor.ini");
+
+	// mock a logged in user by mocking the session and assigning a specefic user to it.
+	// this is only for testing purposes and should not be in the live code.
+	// $_SESSION["profile"] = Profile::getProfileByProfileId($pdo, 732);
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$id = filter_input(INPUT_GET, $id, FILTER_VALIDATE_INT);
+	$reviewStudentProfileId = filter_input(INPUT_GET, "reviewStudentProfileId", FILTER_VALIDATE_INT);
+	$reviewTutorProfileId = filter_input(INPUT_GET, "reviewTutorProfileId", FILTER_VALIDATE_INT);
+
+	// make sure the id is valid for methods that require it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+
+	// handle GET request - if id is present, that review is returned, otherwise all review are returned
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+
+		//get a specific review or all reviews and update reply
+	}
+
 }
