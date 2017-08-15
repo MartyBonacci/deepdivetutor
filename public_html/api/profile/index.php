20c722ed59d449d0c11c0c6c428ea7c3ffdac4bc
Dylan Reuter <djreuter45@gmail.com>
2017-08-15 12:17:26 -0600

    continuing profile api https://app.asana.com/0/333821720990064/401465255199788

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -26,3 +26,28 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
+try {
+	// grab the MySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/deepdivetutor.ini");
+
+	$_SESSION = Profile::getProfileByProfileId($pdo, 1);
+
+	// determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_	METHOD"];
+
+	// sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$profileName = filter_input(INPUT_GET, "profileName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileBio = filter_input(INPUT_GET, "profileBio", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+
+	// make sure the id is valid for methods that require it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+
+	if($method === "GET") {
+
+	}
+}
