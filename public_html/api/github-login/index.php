cf18fde0848a44184bcfa73d969aa63f6f6b3077
Marty Bonacci <martybonacci@gmail.com>
2017-08-19 17:57:20 -0600

    fixed 3 of 4 paths for required files in github-login/index.php https://app.asana.com/0/333821720990064/list

--- public_html/api/github-login/index.php
+++ public_html/api/github-login/index.php
@@ -1,8 +1,8 @@
 <?php
-require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
-require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
+require_once(dirname(__DIR__) . "../../php/classes/autoload.php");
+require_once(dirname(__DIR__) . "../../php/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-require_once(dirname(__DIR__, 4) . "/vendor/autoload.php");
+require_once(dirname(__DIR__) . "../../vendor/autoload.php");
 
 use Edu\Cnm\DeepDiveTutor\ {
 	Profile
