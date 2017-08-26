c8138983d709273bc48d1c8f1b84d2e411cad2d3
Dylan Reuter <djreuter45@gmail.com>
2017-08-26 10:02:06 -0600

    starting image API https://app.asana.com/0/333821720990064/401465255199788

--- public_html/api/images/index.php
+++ public_html/api/images/index.php
@@ -5,4 +5,34 @@ require_once(dirname(__DIR__, 3) . "/php/classes/autoload.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
+use Edu\Cnm\DeepDiveTutor\ {
+	Profile
+};
+
+/**
+ * API for Images
+ *
+ * @author Jack Reuter
+ * @version 1.0
+ */
+
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new StdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	// Grab the MySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/deepdivetutor.ini");
+
+	/**
+	 * Cloudinary API
+	 *
+	 */
+
+}
 
