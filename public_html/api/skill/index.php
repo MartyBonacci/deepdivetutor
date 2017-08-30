bce488c833081a164dfc46db83aee25c2b80b78b
Godfrey Davis <gdavis42@cnm.edu>
2017-08-30 10:58:46 -0600

    changed every instance of $skill id to $id fixed bracket issues with the getby id

--- public_html/api/skill/index.php
+++ public_html/api/skill/index.php
@@ -1,8 +1,8 @@
 <?php
-require_once dirname(__DIR__, 3). "/vendor/autoload.php";
-require_once dirname(__DIR__, 3). "/php/classes/autoload.php";
-require_once dirname(__DIR__, 3). "/php/lib/xsrf.php";
-require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\DeepDiveTutor\{
 	Skill
@@ -15,57 +15,56 @@ use Edu\Cnm\DeepDiveTutor\{
  *
  */
 //verify the session, start if not active
-if(session_status() !== PHP_SESSION_ACTIVE){
+if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 
 //prepare an empty reply
 $reply = new stdClass();
-$reply->status=200;
+$reply->status = 200;
 $reply->data = null;
 
-try{
+try {
 	//grab the mySQL connection
-	$pdo =connectToEncryptedMySQL("/etc/apache2/capstone-mysql/deepdivetutor.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/deepdivetutor.ini");
 	// mock a logged in user by mocking the session and assigning a specific user to it.
 	//this is only for testing purposes and dhould not be in the live code.
 	//$_SESSION["profile"] =$Profile::getProfileByProfileId($pdo, 732);
 
 	//determine which HTTP method was used
-	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"]: $_SERVER["REQUEST_METHOD"];
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
-	$id =filter_input(INPUT_GET,"skillId", FILTER_VALIDATE_INT);
-	$skillName=filter_input(INPUT_GET,"skillName", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
-// make sucre the id is valid for methods that require it
-	if(($method=== "DELETE" || $method=== "PUT")&& (empty($id)=== true || $id < 0)){
-		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
-	}
+	$id = filter_input(INPUT_GET, "skillId", FILTER_VALIDATE_INT);
 
-	if($method==="GET"){
+
+	if($method === "GET") {
 //set xsrf cookie
 		setXsrfCookie();
 
-		//get a specific skillname based on arguments provided or all the skill names and update reply
-		if(empty ($id)===false){
+		//get a specific skillname based on SkillID and update reply
+		if(empty ($id) === false) {
 			$skill = Skill::getSkillNameBySkillId($pdo, $id);
-			if($skill !== null){
-				$reply->data=$skill;
-			}else {
-				$skills = Skill::getAllSkillNames($pdo)->toArray();
-				if($skills !== null){
-					$reply->data = $skills;
-				}
+			if($skill !== null) {
+				$reply->data = $skill;
+			}
+		}
+		//Get All SkillNames and update reply
+		else if(empty($skillName) === false) {
+			$skills = Skill::getAllSkillNames($pdo)->toArray();
+			if($skills !== null) {
+				$reply->data = $skills;
 			}
 		}
 	}
-}catch(\Exception | \TypeError $exception){
-	$reply->status=$exception->getCode();
-	$reply->message=$exception->getMessage();
+
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
 }
 
 header("Content-type:application/json");
-if($reply->data === null){
+if($reply->data === null) {
 	unset($reply->data);
 }
 //encodes json
