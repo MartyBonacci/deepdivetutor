0df91cb6c0e8baff28d38044ce1fa3e204441d86
Dylan Reuter <djreuter45@gmail.com>
2017-08-30 09:30:28 -0600

    added app component https://app.asana.com/0/333821720990064/401465255199788

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector: "angular4-webpack-devserver",
+	templateUrl: "./templates/angular4-webpack-devserver.html"
+})
+
+export class AppComponent {}
