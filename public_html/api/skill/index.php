def51e81f16c4f5d74ee1ea37891b92c6a230e36
Marty Bonacci <martybonacci@gmail.com>
2017-08-30 19:28:20 -0600

    made profile skill post and delete api's https://app.asana.com/0/333821720990064/list

--- public_html/api/skill/index.php
+++ public_html/api/skill/index.php
@@ -5,7 +5,7 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\DeepDiveTutor\{
-	Skill
+	Skill, ProfileSkill
 };
 
 /**
@@ -35,8 +35,9 @@ try {
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
-	$id = filter_input(INPUT_GET, "skillId", FILTER_VALIDATE_INT);
-
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$skillId = filter_input(INPUT_GET, "skillId", FILTER_VALIDATE_INT);
+	$profileId = filter_input(INPUT_GET,"profileId", FILTER_VALIDATE_INT);
 
 	if($method === "GET") {
 //set xsrf cookie
@@ -54,6 +55,74 @@ try {
 			$skills = Skill::getAllSkillNames($pdo)->toArray();
 			if($skills !== null) {
 				$reply->data = $skills;
+			}
+		}
+	}
+
+	// POST new profileSkill
+	if($method === "POST") {
+		//decode the json and turn it into a php object
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//profile id is a required field
+		if(empty($requestObject->profileId) === true) {
+			throw(new \InvalidArgumentException ("No profile id present", 405));
+		}
+
+		//skill id is a required field
+		if(empty($requestObject->skillId) === true) {
+			throw(new \InvalidArgumentException ("No skill id present", 405));
+		}
+
+		// enforce the user is signed in and only trying to edit their own profile
+		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== $profileId) {
+			throw(new \InvalidArgumentException("you are not allowed to access this profile", 403));
+		}
+
+		$count = 0;
+		while($skillId[$count]!== null) {
+			//check if profile skill already exists and if it does not exist, make it
+			$profileSkill = ProfileSkill::getProfileSkillProfileIdAndProfileSkillSkillId($pdo, $profileId, $skillId[$count]);
+			if($profileSkill === null) {
+				$profileSkill = new ProfileSkill($profileId, $skillId[$count]);
+				$profileSkill->insert($pdo);
+			}
+		}
+	}
+
+
+
+
+	if($method === "DELETE") {
+
+		//decode the json and turn it into a php object
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		//profile id is a required field
+		if(empty($requestObject->profileId) === true) {
+			throw(new \InvalidArgumentException ("No profile id present", 405));
+		}
+
+		//skill id is a required field
+		if(empty($requestObject->skillId) === true) {
+			throw(new \InvalidArgumentException ("No skill id present", 405));
+		}
+
+		// enforce the user is signed in and only trying to edit their own profile
+		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== $profileId) {
+			throw(new \InvalidArgumentException("you are not allowed to access this profile", 403));
+		}
+
+		$count = 0;
+		while($skillId[$count]!== null) {
+			//check if profile skill exists and if it does, delete it
+			$profileSkill = ProfileSkill::getProfileSkillProfileIdAndProfileSkillSkillId($pdo, $profileId, $skillId[$count]);
+			if($profileSkill !== null) {
+				$profileSkill = new ProfileSkill($profileId, $skillId);
+				$profileSkill->delete($pdo);
+				$reply->message = "Profile Skill deleted Ok";
 			}
 		}
 	}
