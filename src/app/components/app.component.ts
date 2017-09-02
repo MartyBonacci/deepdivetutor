16b66223919ed68aa92106d29714fc52dee29285
Dylan Reuter <djreuter45@gmail.com>
2017-09-02 12:47:52 -0600

    updated app.component https://app.asana.com/0/333821720990064/401465255199788

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,8 +1,18 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {SessionService} from "../services/session.service";
 
 @Component({
 	selector: "angular4-webpack-devserver",
 	templateUrl: "./templates/angular4-webpack-devserver.html"
 })
 
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+	constructor(protected sessionService: SessionService) {}
+
+	ngOnInit() : void {
+		this.sessionService.setSession();
+	}
+
+
+}
