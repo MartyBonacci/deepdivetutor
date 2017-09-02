cca2deed60eb8c6176fd6f820b1aae4874a7157c
Dylan Reuter <djreuter45@gmail.com>
2017-09-02 11:51:02 -0600

    added session service API https://app.asana.com/0/333821720990064/401465255199788

---
+++ public_html/api/session/index.php
@@ -0,0 +1,23 @@
+<?php
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+	//verify the HTTP method being used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	// if the HTTP method is head check/start the  php session and set the XSRF token
+	if($method === "HEAD") {
+		//verify the session, start if not active
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+			session_start();
+		}
+		setXsrfCookie();
+	} else {
+		throw (new \InvalidArgumentException("attempting to brew coffee with a teapot", 418));
+	}
+} catch(\Exception  | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+echo json_encode($reply);
