f6710a6c6e0d3e93761e10d12e2f9467abe1b7cf
Dylan Reuter <djreuter45@gmail.com>
2017-09-04 11:52:42 -0600

    added more skills to seed data https://app.asana.com/0/333821720990064/401465255199788

--- sql/seed-data.sql
+++ sql/seed-data.sql
@@ -1,7 +1,15 @@
-INSERT INTO skill(skillName)VALUES ('htmlCss');
-INSERT INTO skill(skillName)VALUES ('bootstrap');
-INSERT INTO skill(skillName)VALUES ('jquery');
-INSERT INTO skill(skillName)VALUES ('php');
-INSERT INTO skill(skillName)VALUES ('mysqlDatabase');
-INSERT INTO SKILL(skillName)VALUES ('javaScript');
+INSERT INTO skill(skillName)VALUES ('HTML/CSS');
+INSERT INTO skill(skillName)VALUES ('Bootstrap');
+INSERT INTO skill(skillName)VALUES ('jQuery');
+INSERT INTO skill(skillName)VALUES ('PHP');
+INSERT INTO skill(skillName)VALUES ('MySQL');
+INSERT INTO skill(skillName)VALUES ('JavaScript');
+INSERT INTO skill(skillName)VALUES ('AJAX');
+INSERT INTO skill(skillName)VALUES ('OOP');
+INSERT INTO skill(skillName)VALUES ('Angular');
+INSERT INTO skill(skillName)VALUES ('Java');
+INSERT INTO skill(skillName)VALUES ('.NET');
+INSERT INTO skill(skillName)VALUES ('Android');
+INSERT INTO skill(skillName)VALUES ('Organization');
+INSERT INTO skill(skillName)VALUES ('Time Management');
 
