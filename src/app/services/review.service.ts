e5da17fe05104774f6ab5f95101a1a85351e00dc
Tim Williams <tikytimbo5@yahoo.com>
2017-09-06 16:44:55 -0600

    completed review.service.ts https://app.asana.com/0/333821720990064/401465255199790/f

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -1,4 +1,45 @@
 import {Injectable} from "@angular/core";
 import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base.service";
 import {Review} from "../classes/review";
+
+@Injectable()
+export class ReviewService extends BaseService {
+	constructor(protected http: Http) {
+		super(http);
+	}
+
+	private reviewUrl = "api/review/";
+
+	getReviewByReviewId(reviewId: number) : Observable<Review> {
+		return(this.http.get(this.reviewUrl + reviewId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getReviewByReviewStudentProfileId(reviewStudentProfileId: number) : Observable<Review> {
+		return(this.http.get(this.reviewUrl + reviewStudentProfileId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getReviewByReviewTutorProfileId(reviewTutorProfileId: number) : Observable<Review> {
+		return(this.http.get(this.reviewUrl + reviewTutorProfileId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getReviewByReviewText(reviewText: string) : Observable<Review> {
+		return(this.http.get(this.reviewUrl + reviewText)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	createReview(profile: Review) : Observable<Review> {
+		return(this.http.post(this.reviewUrl, profile)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+
+}
