4465da0bf807a7dce899259876e343cb6c8f7ae5
Tim Williams <tikytimbo5@yahoo.com>
2017-09-06 15:10:58 -0600

    added browse components https://app.asana.com/0/333821720990064/401465255199790/f

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
 import {ProfileComponent} from "./components/profile.component";
+import {BrowseComponent} from "./components/browse.component"
 import {SessionService} from "./services/session.service";
 import {SkillService} from "./services/skill.service";
 
 
-export const allAppComponents = [HomeComponent, ProfileComponent];
+export const allAppComponents = [HomeComponent, ProfileComponent, BrowseComponent];
 
 export const routes: Routes = [
+	{path: "", component: HomeComponent},
 	{path: "profile", component: ProfileComponent},
-	{path: "", component: HomeComponent}
+	{path: "browse", component: BrowseComponent}
 ];
 
 export const appRoutingProviders: any[] = [SessionService];
