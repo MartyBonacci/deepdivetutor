89052852a1e41333b74256e616fb4975f2ebc9b7
Dylan Reuter <djreuter45@gmail.com>
2017-09-07 13:12:23 -0600

    added navbar component

---
+++ src/app/components/navbar.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Observable} from "rxjs";
+
+@Component({
+	selector: "navbar",
+	templateUrl: "./templates/navbar.html"
+})
+
+export class NavbarComponent {}
