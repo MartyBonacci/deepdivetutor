7fc53c2657ac1e7d74951e5c8745d3679af7a1e6
Dylan Reuter <djreuter45@gmail.com>
2017-09-07 13:12:36 -0600

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,21 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home.component";
 import {ProfileComponent} from "./components/profile.component";
-import {BrowseComponent} from "./components/browse.component"
+import {BrowseComponent} from "./components/browse.component";
+import {NavbarComponent} from "./components/navbar.component";
 import {SessionService} from "./services/session.service";
 
 export const allAppComponents = [
 	HomeComponent,
 	ProfileComponent,
-	BrowseComponent
+	BrowseComponent,
+	NavbarComponent
 ];
 
 export const routes: Routes = [
-	{path: "", component: HomeComponent},
+	{path: "browse", component: BrowseComponent},
 	{path: "profile", component: ProfileComponent},
-	{path: "browse", component: BrowseComponent}
+	{path: "", component: HomeComponent}
 ];
 
 export const appRoutingProviders: any[] = [SessionService];
