3d0b47dba1b88bc85d3e692ce17d49667025ddca
Godfrey Davis <gdavis42@cnm.edu>
2017-09-07 15:37:48 -0600

    put in the footer component and template

---
+++ src/app/components/footer.component.ts
@@ -0,0 +1,10 @@
+import {Component,OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import{Observable} from "rxjs";
+
+@Component({
+	selector: "app-footer",
+	templateUrl:"./templates/footer.php"
+})
+
+export class FooterComponent{}
