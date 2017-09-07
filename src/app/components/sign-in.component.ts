9f3ee3339c4c1c4eafd8d954b254e41981c2c473
Godfrey Davis <gdavis42@cnm.edu>
2017-09-07 15:14:50 -0600

    sign in fixed for the sign in component

--- src/app/components/sign-in.component.ts
+++ src/app/components/sign-in.component.ts
@@ -3,13 +3,13 @@ import {Component, ViewChild, EventEmitter, Output} from "@angular/core";
 import {Router} from "@angular/router";
 import {Observable} from "rxjs/Observable";
 import {Status} from "../classes/status";
-import {SignInSevice} from "../services/sign-in.service"
+import {SignInService} from "../services/sign-in.service"
 import {SignIn} from "../classes/sign-in"
 declare var $: any;
 
 @Component({
-	templateUrl: "./templates/signin.php",
-	selector:"isgnin"
+	templateUrl: "./templates/signin.html",
+	selector:"signin"
 })
 
 export class SignInComponent {
@@ -18,7 +18,22 @@ export class SignInComponent {
 	signin: SignIn = new SignIn(null, null);
 	status: Status = null;
 
-	constructor(private SignInService: SignInService, private Router: Router){
-
+	constructor(private SignInService: SignInService, private router: Router){
 	}
+
+	signIn(): void {
+		this.SignInService.createSignIn(this.signin).subscribe(status=>{
+			this.status=status;
+			if(status.status === 200){
+
+				this.router.navigate([""]);
+				this.signInForm.reset();
+				setTimeout(function(){$("#signin-modal").modal('hide');},1000);
+			}else{
+				console.log("failed login")
+			}
+		});
+	}
+
 }
+
