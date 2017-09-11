a3394abc13bb5a664f6bb2aa01ad6d167ee8a4a0
Tim Williams <tikytimbo5@yahoo.com>
2017-09-11 09:41:20 -0600

    angular components for browse https://app.asana.com/0/333821720990064/401465255199790/f

--- src/app/components/browse.component.ts
+++ src/app/components/browse.component.ts
@@ -1,7 +1,29 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router"
+import {ProfileService} from "../services/profile.service";
+import {subscribeOn} from "rxjs/operator/subscribeOn";
+import {Status} from "../classes/status";
+import {Profile} from "../classes/profile";
+
 
 @Component({
-	templateUrl: "./templates/browse.html"
+	templateUrl: "./templates/browse.html",
+	providers: [ProfileService]
 })
 
-export class BrowseComponent {}
+export class BrowseComponent implements OnInit {
+	tutorProfiles: Profile[]= [];
+	tutorProfile: Profile= new Profile(null,null,null,null,null,null,null,null,null,null,null,null);
+	status: Status = null;
+
+	constructor( private profilesService: ProfileService, private router:Router){}
+
+	ngOnInit() : void {
+		this.reloadProfiles();
+	}
+
+	reloadProfiles(): void {
+			this.profilesService.getProfileByProfileType(1)
+				.subscribe(tutorProfiles=>this.tutorProfiles = tutorProfiles);
+		}
+}
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -12,25 +12,25 @@ export class ProfileService extends BaseService {
 
 	private profileUrl = "api/profile/";
 
-	getProfileByProfileId(profileId: number) : Observable<Profile> {
+	getProfileByProfileId(profileId: number) : Observable<Profile[]> {
 		return(this.http.get(this.profileUrl + profileId)
 			.map(this.extractData)
 			.catch(this.handleError));
 	}
 
-	getProfileByProfileName(profileName: string) : Observable<Profile> {
+	getProfileByProfileName(profileName: string) : Observable<Profile[]> {
 		return(this.http.get(this.profileUrl + "?profileName=" + profileName)
 			.map(this.extractData)
 			.catch(this.handleError));
 	}
 
-	getProfileByProfileRate(brokeProfileRate: number, loadedProfileRate: number) : Observable<Profile> {
+	getProfileByProfileRate(brokeProfileRate: number, loadedProfileRate: number) : Observable<Profile[]> {
 		return(this.http.get(this.profileUrl + "?brokeProfileRate" + brokeProfileRate + "&loadedProfileRate=" + loadedProfileRate)
 			.map(this.extractData)
 			.catch(this.handleError));
 	}
 
-	getProfileByProfileType(profileType: number) : Observable<Profile> {
+	getProfileByProfileType(profileType: number) : Observable<Profile[]> {
 		return(this.http.get(this.profileUrl)
 			.map(this.extractData)
 			.catch(this.handleError));
