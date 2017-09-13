d4daa9d5527c74e7d282df23736bcc17f018db16
Tim Williams <tikytimbo5@yahoo.com>
2017-09-13 10:07:16 -0600

    added route profile template https://app.asana.com/0/333821720990064/401465255199790/f

---
+++ src/app/components/rtprofile.component.ts
@@ -0,0 +1,46 @@
+///<reference path="../services/profile.service.ts"/>
+import {Component, OnInit} from "@angular/core";
+import {Profile} from "../classes/profile";
+import {ProfileService} from "../services/profile.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Status} from "../classes/status";
+import {SessionService} from "../services/session.service";
+import {CookieService} from "ng2-cookies";
+
+
+
+
+
+@Component({
+	templateUrl: "./templates/profile.html",
+	providers: [ProfileService]
+})
+
+export class ProfileComponent implements OnInit {
+	profiles: Profile[]=[];
+	profile: Profile= new Profile(null,null,null,null,null,null,null,null,null);
+	status: Status = null;
+
+	cookieJar : any = {};
+
+	constructor( private profileService:ProfileService, private router:Router, private route:ActivatedRoute, protected cookieService: CookieService, protected sessionService: SessionService){}
+
+	ngOnInit() : void {
+		this.route.params.forEach((params: Params) => {
+			let profileId = +params["profileId"];
+			this.cookieJar = this.cookieService.getAll();
+			this.profileService.getProfile(profileId)
+				.subscribe(profile => this.profile = profile);
+
+		})
+	}
+	reloadProfile(): void{
+		this.sessionService.setSession();
+		this.cookieJar = this.cookieService.getAll();
+		this.profileService.getProfileByProfileId(this.cookieJar['profileId'])
+			.subscribe (profiles=>this.profiles=profiles);
+	}
+	switchProfile(profile:Profile): void{
+		this.router.navigate(["/profile/", profile.profileId]);
+	}
+}
