a5f23b6b09ca8a4e341e9e83c595a67d77ebf266
Godfrey Davis <gdavis42@cnm.edu>
2017-09-14 10:44:25 -0600

    changed the wor

--- src/app/components/review.component.ts
+++ src/app/components/review.component.ts
@@ -31,7 +31,7 @@ export class ReviewComponent implements OnInit {
 	reloadReviews(): void {
 		this.sessionService.setSession();
 		this.cookieJar = this.cookieService.getAll();
-		this.reviewService.getReviewByReviewTutorProfileId(this.cookieJar['profileId'])
+		this.reviewService.getReviewByReviewTutorProfileId(255)
 			.subscribe(reviews => this.reviews = reviews);
 	}
 
